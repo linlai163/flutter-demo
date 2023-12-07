539da7528ff2380a00fd18007f25261c48e33b28
chao.xu2 <chao.xu2@cerence.com>
2023-12-07 13:27:50 +0800

    feat: scroll listener demo page

---
+++ lib/pages/scroll_listener_demo_page/scroll_listener_demo_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class ScrollListenerDemoPage extends StatefulWidget {
+  const ScrollListenerDemoPage({super.key});
+
+  @override
+  State<ScrollListenerDemoPage> createState() => _ScrollListenerDemoPageState();
+}
+
+class _ScrollListenerDemoPageState extends State<ScrollListenerDemoPage> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool isEnd = false;
+
+  double offset = 0;
+
+  String notify = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        offset = _scrollController.offset;
+        isEnd = _scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ScrollListenerDemoPage")),
+      body: Container(
+        alignment: Alignment.center,
+        child: NotificationListener(
+          onNotification: (dynamic notification) {
+            String notify = "";
+            if (notification is ScrollNotification) {
+              notify = "scroll end";
+            } else if (notification is ScrollStartNotification) {
+              notify = "scroll start";
+            } else if (notification is UserScrollNotification) {
+              notify = "user scroll";
+            } else if (notification is ScrollUpdateNotification) {
+              notify = "scroll update";
+            }
+            setState(() {
+              this.notify = notify;
+            });
+            return false;
+          },
+          child: ListView.builder(
+            controller: _scrollController,
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return Container(
+                height: 60,
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              );
+            },
+          ),
+        ),
+      ),
+      persistentFooterButtons: <Widget>[
+        TextButton(
+          onPressed: () {
+            _scrollController.animateTo(0,
+                duration: const Duration(seconds: 3),
+                curve: Curves.bounceInOut);
+          },
+          child: Text("Position: ${offset.floor()}"),
+        ),
+        const SizedBox(
+          width: 0.3,
+          height: 0.3,
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(notify),
+        ),
+        Visibility(
+            visible: isEnd,
+            child: TextButton(
+              child: const Text('到达底部'),
+              onPressed: () {},
+            ))
+      ],
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -3,8 +3,9 @@ import 'package:flutter_pratical_experience/widgets/container_async_route_page.d
 
 import 'pages/controller_demo_page/controller_demo_page.dart'
     deferred as controller_demo_page;
-
 import 'pages/clip_demo_page/clip_demo_page.dart' deferred as clip_demo_page;
+import 'pages/scroll_listener_demo_page/scroll_listener_demo_page.dart'
+    deferred as scroll_listener_demo_page;
 
 Map<String, WidgetBuilder> routers = {
   "文本输入框简单的 Controller": (context) {
@@ -17,5 +18,11 @@ Map<String, WidgetBuilder> routers = {
     return ContainerAsyncRouterPage(clip_demo_page.loadLibrary(), (context) {
       return clip_demo_page.ClipDemoPage();
     });
-  }
+  },
+  "列表滑动监听": (context) {
+    return ContainerAsyncRouterPage(scroll_listener_demo_page.loadLibrary(),
+        (context) {
+      return scroll_listener_demo_page.ScrollListenerDemoPage();
+    });
+  },
 };

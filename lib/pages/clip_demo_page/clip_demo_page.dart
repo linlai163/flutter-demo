43eedcc9aef74569cf85a978e2bdb09685917c2c
chao.xu2 <chao.xu2@cerence.com>
2023-12-07 13:08:20 +0800

    feat: clip demo page

---
+++ lib/pages/clip_demo_page/clip_demo_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ClipDemoPage extends StatelessWidget {
+  const ClipDemoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ClipDemoPage")),
+      body: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("BoxDecoration 圆角"),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: AssetImage("images/cat.png")),
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+            ),
+            const SizedBox(height: 10),
+            const Text("BoxDecoration 圆角 child"),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              child: Image.asset(
+                "images/cat.png",
+                fit: BoxFit.cover,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text("ClipRect 圆角对 child"),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+              child: Image.asset(
+                "images/cat.png",
+                fit: BoxFit.cover,
+                width: 100,
+                height: 100,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,8 @@ import 'package:flutter_pratical_experience/widgets/container_async_route_page.d
 import 'pages/controller_demo_page/controller_demo_page.dart'
     deferred as controller_demo_page;
 
+import 'pages/clip_demo_page/clip_demo_page.dart' deferred as clip_demo_page;
+
 Map<String, WidgetBuilder> routers = {
   "文本输入框简单的 Controller": (context) {
     return ContainerAsyncRouterPage(controller_demo_page.loadLibrary(),
@@ -11,4 +13,9 @@ Map<String, WidgetBuilder> routers = {
       return controller_demo_page.ControllerDemoPage();
     });
   },
+  "实现控件圆角不同组合": (context) {
+    return ContainerAsyncRouterPage(clip_demo_page.loadLibrary(), (context) {
+      return clip_demo_page.ClipDemoPage();
+    });
+  }
 };
